bb63530e005b0543fabd70bb9915ab2d7ecdf0b7
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-02-07 21:39:39 +0900

    add ApcSsrModule module

---
+++ src/ApcSsrModule.php
@@ -0,0 +1,29 @@
+<?php
+declare (strict_types = 1);
+
+/**
+ * This file is part of the BEAR\ReactJsModule package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\SsrModule;
+
+use BEAR\SsrModule\Annotation\SsrCacheConfig;
+use Psr\SimpleCache\CacheInterface;
+use Ray\Di\AbstractModule;
+use Symfony\Component\Cache\Simple\ApcuCache;
+
+class ApcSsrModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        // bind ssr cache
+        $this->bind(CacheInterface::class)->annotatedWith(SsrCacheConfig::class)->to(ApcuCache::class);
+        // install module
+        $this->install(new CacheSsrModule());
+        $this->install(new SsrModule(__DIR__ . '/Fake/build'));
+    }
+}
