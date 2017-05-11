917f01f9d73be4e252f62e37550e3d49da08ab1b
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-11 11:23:51 +0900

    add StatusKeyNotExistsException and MetaKeyNotExistsException

--- tests/Fake/FakeRo.php
+++ tests/Fake/FakeRo.php
@@ -8,12 +8,13 @@ use BEAR\SsrModule\Annotation\Ssr;
 class FakeRo extends ResourceObject
 {
     /**
-     * @Ssr(app="test_ssr", state={"name"})
+     * @Ssr(app="test_ssr", state={"name"}, metas={"title"})
      */
     public function onGet()
     {
         $this->body = [
-            'name' => 'World'
+            'name' => 'World',
+            'title' => 'Title'
         ];
 
         return $this;
--- tests/SsrModuleTest.php
+++ tests/SsrModuleTest.php
@@ -2,12 +2,24 @@
 
 namespace BEAR\SsrModule;
 
+use BEAR\SsrModule\Exception\StatusKeyNotExistsException;
 use Koriym\Baracoa\Baracoa;
 use Koriym\Baracoa\BaracoaInterface;
 use Ray\Di\Injector;
 
 class SsrModuleTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var FakeRo
+     */
+    private $ro;
+
+    protected function setUp()
+    {
+        $module = new SsrModule(__DIR__ . '/Fake/build');
+        $this->ro = (new Injector($module))->getInstance(FakeRo::class);
+    }
+
     public function testGetInstance()
     {
         $module = new SsrModule(__DIR__ . '/Fake/build');
@@ -46,6 +58,28 @@ class SsrModuleTest extends \PHPUnit_Framework_TestCase
     public function testNoAppName(FakeRo $ro)
     {
         $ro->onNoApp();
+        $ro->toString();
+    }
+
+    /**
+     * @expectedException \BEAR\SsrModule\Exception\StatusKeyNotExistsException
+     */
+    public function testNoStatusException()
+    {
+        $ro = clone $this->ro;
+        $ro->onGet();
+        $ro->body = ['title' => 'exsits'];
+        $ro->toString();
+    }
+
+    /**
+     * @expectedException \BEAR\SsrModule\Exception\MetaKeyNotExistsException
+     */
+    public function testMetaStatusNotExistsException()
+    {
+        $ro = clone $this->ro;
+        $ro->onGet();
+        $ro->body = ['name' => 'exsits'];
         $ro->toString();
     }
 }
